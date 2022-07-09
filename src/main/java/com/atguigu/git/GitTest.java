9aed0d8fcfffab8d6e4ce1b4263ee0e48808a3bc
Pree <Pree@atguigu.com>
2022-07-09 15:33:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Pree
+ * @create 2022-07-09 15:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
